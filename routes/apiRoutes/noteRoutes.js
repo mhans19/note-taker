f93fb23f4759db10be33d0a5ab23d1a946cd5d53
mhans19 <mdhansen19@gmail.com>
2020-07-12 14:14:10 -0600

    created functions for get and post requests

--- routes/apiRoutes/noteRoutes.js
+++ routes/apiRoutes/noteRoutes.js
@@ -1,9 +1,35 @@
 // DEPENDENCIES
 const router = require("express").Router(); // Express Routers
-const { } = require("../../lib/notes"); // GET/POST functions
-const { db } = require("../../db/db"); // json housing data
+const { filterByQuery, findById, validateNote, createNewNote } = require("../../lib/notes"); // GET/POST functions
+const { notes } = require("../../db/db"); // json housing data
 // GET/POST ROUTES
     // GET ROUTES
-
+    // Filter Query GET
+    router.get("/notes", (req, res) => {
+        let results = notes;
+        if (req.query) {
+            results = filterByQuery(req.query, results);
+        }
+        res.json(results);
+    });
+    // Filter by ID GET
+    router.get("/notes/:id", (req, res) => {
+        const result = findById(req.params.id, notes);
+        if (result) {
+            res.json(result);
+        } else {
+            res.send(404);
+        }
+    });
     // POST ROUTES
+    router.post("/notes", (req,res) => {
+        req.body.id = notes.length.toString();
+        if (!validateNote(req.body)) {
+            res.status(400).send("The note is not properly formatted.");
+        } else {
+            const note = createNewNote(req.body, notes);
+            res.json(note);
+        }
+    });
 // MODULE EXPORTS
+module.exports = router;
--- routes/htmlRoutes/index.js
+++ routes/htmlRoutes/index.js
@@ -4,10 +4,10 @@ const router = require('express').Router();
 // ROUTES
     // Front-end, HTML files (public folder)
     router.get('/', (req, res) => {
-        res.sendFile(path.join(_dirname, '../../public/index.html'));
-    });
-    router.get('/notes', (req, res) => {
-        res.sendFile(path.join(_dirname, '../../public/notes.html'));
-    });
+        res.sendFile(path.join(__dirname, '../../public/index.html'));
+      });
+      router.get('/notes', (req, res) => {
+        res.sendFile(path.join(__dirname, '../../public/notes.html'));
+      });
 // MODULE EXPORTS
     module.exports = router;
