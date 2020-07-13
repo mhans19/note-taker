10d5d996f144408bdf125d177b62dfe7faee232f
mhans19 <mdhansen19@gmail.com>
2020-07-12 22:21:15 -0600

    delete note router done

--- routes/apiRoutes/noteRoutes.js
+++ routes/apiRoutes/noteRoutes.js
@@ -1,29 +1,43 @@
 // DEPENDENCIES
 const router = require("express").Router(); // Express Routers
 const { findById, validateNote, createNewNote } = require("../../lib/notes"); // GET/POST functions
-const db = require("../../db/db"); // json housing data
-const { fs } = require("fs");
-const notes = require("../../lib/notes");
+const notes = require("../../db/db"); // json housing data
+const fs = require("fs");
+const path = require("path");
+
 // GET/POST ROUTES
     // GET ROUTES
     // Filter Query GET
     router.get("/notes", (req, res) => {
-        let results = db;
+        let results = notes;
         res.json(results);
     });
     // POST ROUTES
     router.post("/notes", (req, res) => {
-        req.body.id = db.length.toString();
+        req.body.id = notes.length.toString();
         if (!validateNote(req.body)) {
             res.status(400).send("The note is not properly formatted.");
         } else {
-            const note = createNewNote(req.body, db);
+            const note = createNewNote(req.body, notes);
             res.json(note);
         }
     });
     // Filter by ID to Delete object
-    // router.delete("/notes/:id", (req, res) => {
-    //     db.splice(req.params.id, 1);
-    // });
+    router.delete("/notes/:id", (req, res) => {
+        const result = findById(req.params.id, notes);
+        if (result) {
+          res.json(result);
+        } else {
+          res.send(404);
+        }
+
+        notes.splice(result,1);
+
+        fs.writeFileSync(
+            path.join(__dirname, '../../db/db.json'),
+            JSON.stringify(notes, null, 2)
+        );
+    });
+
 // MODULE EXPORTS
 module.exports = router;
